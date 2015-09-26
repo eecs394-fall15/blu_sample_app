b4bd5f5e168cf249eebb3143a5e10e3df857ca00
aforaleka <aforaleka@gmail.com>
2015-09-25 20:27:44 -0500

    improved camera function and replaced camera icon with new photo on new-card view

--- app/example/scripts/NewCardController.js
+++ app/example/scripts/NewCardController.js
@@ -10,15 +10,29 @@ angular
     	alert("test");
     };
 
-	var options = {
-	  targetHeight: 300,
-	  encodingType: "png",
-	};
+//Camera functions
 
 	$scope.newCard = function() {
-	  supersonic.media.camera.takePicture(options).then( function(result){
-	    $scope.image = result
-	  });
-	};
+		navigator.camera.getPicture(onSuccess, onFail, { quality: 75,
+	    destinationType: Camera.DestinationType.DATA_URL, 
+	    targetWidth: 500, targetHeight: 250
+		});
+	}
+
+	function onSuccess(imageData) {
+	    var image = document.getElementById('cardImage');
+	    image.src = "data:image/jpeg;base64," + imageData;
+	}
+
+	function onFail() {
+	    alert('Take a picture of the business card :)');
+	}
+
+	//form for entering contact info for card
+	$scope.master = {};
+
+	$scope.update = function(contact) {
+        $scope.master = angular.copy(contact);
+      };
 
   });
