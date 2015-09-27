ba81d35469428228fa867a2eafd4b11d9f8f38de
fparedesg <fparedesgarza@gmail.com>
2015-09-27 14:57:44 -0500

    Added logic for when users edit exitsing cards. Changed image references to be the id instead of the name. The code now saves the date when the card was added on the dateId column in the form 'yyyymmdd'

--- app/card/scripts/EditController.js
+++ app/card/scripts/EditController.js
@@ -14,9 +14,43 @@ angular
 
     $scope.submitForm = function() {
       $scope.showSpinner = true;
-      $scope.card.save().then( function () {
-        supersonic.ui.modal.hide();
+
+      var Cards = supersonic.data.model("Card");
+      Cards.findAll().then( function(allCards) {
+
+        // Check if there is a name on the name field. Return and warn user if not.
+        if(!$scope.card.name) {
+          supersonic.ui.dialog.alert("Please input a name.");
+          document.getElementsByName('nameLabel')[0].style.backgroundColor = "#FFB8B6";
+          return;
+        }
+
+        // Logic to check if name is already in database.
+        var exists = false;
+        for(var i = 0; i < allCards.length; i++) {
+          // Make sure to allow the same name to be reused (i.e. the second part of the conditional)
+          if(allCards[i].name == $scope.card.name && allCards[i].id != $scope.card.id) {
+            exists = true;
+          }
+        }
+        
+        // If name is already in database, return and warn user.
+        if(exists) {
+          supersonic.ui.dialog.alert("Name already exists.\nPlease input another name.");
+          return;
+        }
+
+        // If no problems were encountered, save new card.
+        $scope.card.save().then( function () {
+          supersonic.ui.modal.hide();
+        }, function () {
+          // This is necessary. If the promise fails (i.e. the card doesn't save), the modal still has to close.
+          // This ALWAYS happens if the user clicks on "Edit Card", but makes no changes and still presses "Save".
+          supersonic.ui.modal.hide();
+        });
       });
+
+      $scope.showSpinner = false;
     }
 
     $scope.cancel = function () {
