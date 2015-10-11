4a83a6e27b5b71982a91c4955e5dc87851286689
aforaleka <aforaleka@gmail.com>
2015-10-11 17:03:35 -0500

    added angular-carousel

--- app/common/index.js
+++ app/common/index.js
@@ -2,7 +2,8 @@ angular.module('common', [
   // Declare here all AngularJS dependencies that are shared by all modules.
   'supersonic',
   'parse-angular',
-  'parse-angular.enhance'
+  'parse-angular.enhance',
+  'angular-carousel'
   ]).run( function() {
 	Parse.initialize("yqtXuPcMyUFfsvxLL79AeNLCgDJ3G7DX9zUhz6IX", "fq7twRB4J99HaNdUpCQ5OtTN0nHbI0nd9QmLvPws");
 });
